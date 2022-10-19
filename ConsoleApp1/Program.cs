a9b4b5f121583c9df5a09758bdc4c255ff3064ff
Uthef <denbrylski@gmail.com>
2022-10-19 17:06:31 +0300

    Redundant projects removed

--- ConsoleApp1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using MusicResolver;
-
-Console.WriteLine("Hello, World!");
-
-var result = await new MainMusicResolver(new ResolverConfiguration()).SearchBandcamp("da", ItemType.Track);
-Console.WriteLine(result.Count);
